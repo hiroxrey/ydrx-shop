a20e53efb9d5e23b082b1dd85b00fdee1c7f0235
hiroxrey <hiroxrey.lzx@gmail.com>
2025-12-20 00:21:51 -0600

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,7 @@
-// supabase.js
-const SUPABASE_URL = "https://berln1zntgf8ry1qugdk.supabase.co";
+const SUPABASE_URL = "https://berlnlzntgfrgylqugdk.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_qt55KxqxupDWi_RdO0Nm6g_A3n5V1yt";
 
-window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
